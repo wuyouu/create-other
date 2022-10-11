738fbae433a2803e6e9ec0fe6c9446ce778a36c3
995347291@qq.com <995347291@qq.com>
2022-10-11 14:20:49 +0800

    init

---
+++ q.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    type: 'confirm',
+    name: 'private',
+    message: '是否私有?'
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: '作者是?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: '描述信息？'
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'license?'
+  },
+]
